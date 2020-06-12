0c57219a97caadf72613167043dd55d3442ec7dc
ujjwal66 <bindasujjwal@gmail.com>
2020-06-12 23:25:33 +0545

    security issue

--- bundle.js
+++ bundle.js
@@ -1,8 +1,4 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-var _ =require('lodash');
 
-var array=[1,2,3,4,5,6];
-console.log('answer:',_.without(array,6));
 var color1=document.querySelector(".color1");
 var color2=document.querySelector(".color2");
 var gradientname=document.querySelector("h3");
